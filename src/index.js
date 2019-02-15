29836e0f144712419d5f2467f126610c5229f4c7
niechao <chao.nie@starwin.com>
2019-02-15 15:58:24 +0800

    fix add dependency commander

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 #!/usr/bin/env node
 
 console.log("*******************HELLO THERE***************");
+
+const path = require('path');
+const fs = require('fs');
+const program = require('commander');
+
+class FileNode {
+    constructor(type, name, absolutePath) {
+        this.type = type;
+        this.name = name;
+        this.absolutePath = absolutePath;
+        if (type === 'DIR') {
+            this.children = [];
+        }
+    }
+}
+
+function listDirectoryOrFileRecursive(targetPath) {
+    var absoluteTargetPath = path.resolve(targetPath);
+    var rootDir = path.parse(absoluteTargetPath);
+    var root;
+    var stats = fs.statSync(absoluteTargetPath);
+    if (stats.isDirectory()) {
+        root = new FileNode('DIR', rootDir.base, absoluteTargetPath);
+        root = listDirectory(absoluteTargetPath, root);
+    } else if (stats.isFile()) {
+        root = new FileNode('FILE', rootDir.base, absoluteTargetPath);
+    }
+    return root;
+}
+
 function listDirectory(targetPath, parentNode) {
     var files = fs.readdirSync(targetPath, {
         withFileTypes: true
@@ -19,33 +49,18 @@ function listDirectory(targetPath, parentNode) {
     return parentNode;
 }
 
-function FileNode(type, name, absolutePath) {
-    this.type = type;
-    this.name = name;
-    this.absolutePath = absolutePath;
-    if (type === 'DIR') {
-        this.children = [];
-    }
-}
+program
+    .version('0.0.2')
+    .usage('<command> [options]');
 
-var path = require('path');
-const fs = require('fs');
+program
+    .command('list [dir]')
+    .alias('ls')
+    .description('列出当前目录下所有目录及文件')
+    .action(function (dir) {
+        const result = listDirectoryOrFileRecursive(dir);
+        console.log(result);
+    });
 
-var argvs = process.argv.slice(2);
-var targetPath = process.cwd();
-if (argvs.length) {
-    targetPath = argvs[0];
-}
-var absoluteTargetPath = path.resolve(targetPath);
-var rootDir = path.parse(absoluteTargetPath);
-var root;
-var stats = fs.statSync(absoluteTargetPath);
-if (stats.isDirectory()) {
-    root = new FileNode('DIR', rootDir.base, absoluteTargetPath);
-    root = listDirectory(absoluteTargetPath, root);
-} else if (stats.isFile()) {
-    root = new FileNode('FILE', rootDir.base, absoluteTargetPath);
-}
-// fs.writeFileSync(process.cwd() + path.sep + root.name + '.json', JSON.stringify(root));
-console.log(root);
+program.parse(process.argv);
 console.log("*******************SUCCESS***************");
